2e40f5a0e11d7d4b179467accdf2dabeac9da443
MageshCM <164765537+MageshCM@users.noreply.github.com>
2025-12-25 20:14:21 +0530

    Update main.py

--- innofeed-backend/main.py
+++ innofeed-backend/main.py
@@ -116,9 +116,11 @@ origins = [
     "http://localhost:5173",
 ]
 
+
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
